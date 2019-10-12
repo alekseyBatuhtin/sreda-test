f3a4847cc3bd9d5b6ced32b801e5ba0545085daf
Alexey Batuhtin <full.naked.eye@gmail.com>
2019-10-12 14:30:19 +0700

    add ui kit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { CSSReset, ThemeProvider } from '@chakra-ui/core';
+import SearchRepositories from './components/SearchRepositories';
 
 function App() {
   return (
-    <div />
+    <ThemeProvider>
+      <CSSReset />
+      <SearchRepositories />
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/SearchRepositories/SearchRepositories.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function SearchRepositories() {
+  return (
+    <div>
+      {/* <Search />
+      <RepositoriesList />
+      <Pagination /> */}
+    </div>
+  );
+}
+
+
+export default SearchRepositories;
