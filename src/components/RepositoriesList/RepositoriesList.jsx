a72d0348c0b8f9a6ed6c5304e544b217d841a0eb
Alexey Batuhtin <full.naked.eye@gmail.com>
2019-10-13 12:07:30 +0700

    replace RepositoryItem as single component

--- src/components/RepositoriesList/RepositoriesList.jsx
+++ src/components/RepositoriesList/RepositoriesList.jsx
@@ -1,48 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-  Box, Text, Spinner, Link,
-} from '@chakra-ui/core';
-import { ReactComponent as StarIcon } from '../../icons/star.svg';
-import { ReactComponent as RepoIcon } from '../../icons/repo.svg';
+import { Box, Spinner } from '@chakra-ui/core';
 
-const RepositoryItem = ({
-  name,
-  login,
-  description,
-  url,
-  licenseInfo,
-  starsCount,
-  createdAt,
-}) => (
-  <Box
-    borderWidth="1px"
-    rounded="lg"
-    width="30%"
-    minWidth="280px"
-    marginBottom="20px"
-    marginX="10px"
-    padding="8px"
-    flexBasis="30%"
-    flexGrow="1"
-  >
-    <Link href={url} color="#0366d6" fontSize="lg">
-      <Text>
-        <Box as={RepoIcon} display="inline" marginRight="8px" />
-        {`${login}/${name}`}
-      </Text>
-    </Link>
-    {description && <Text fontSize="sm" lineHeight="1.25">{description}</Text>}
-    <Box display="flex" alignItems="center" flexWrap="wrap" color="gray.600" marginTop="12px">
-      <Text display="inline" marginRight="8px">
-        <Box as={StarIcon} display="inline" />
-        {starsCount}
-      </Text>
-      {licenseInfo && <Text marginRight="8px">{licenseInfo.name}</Text>}
-      <Text marginRight="8px">{`Created: ${createdAt}`}</Text>
-    </Box>
-  </Box>
-);
+import RepositoryItem from '../RepositoryItem';
 
 const RepositoriesList = ({ fetch, repositories }) => (
   <Box
---
+++ src/components/RepositoryItem/RepositoryItem.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Box, Text, Link } from '@chakra-ui/core';
+import { ReactComponent as StarIcon } from '../../icons/star.svg';
+import { ReactComponent as RepoIcon } from '../../icons/repo.svg';
+
+const RepositoryItem = ({
+  name,
+  login,
+  description,
+  url,
+  licenseInfo,
+  starsCount,
+  createdAt,
+}) => (
+  <Box
+    borderWidth="1px"
+    rounded="lg"
+    width="30%"
+    minWidth="280px"
+    marginBottom="20px"
+    marginX="10px"
+    padding="8px"
+    flexBasis="30%"
+    flexGrow="1"
+  >
+    <Link href={url} color="#0366d6" fontSize="lg">
+      <Text>
+        <Box as={RepoIcon} display="inline" marginRight="8px" />
+        {`${login}/${name}`}
+      </Text>
+    </Link>
+    {description && <Text fontSize="sm" lineHeight="1.25">{description}</Text>}
+    <Box display="flex" alignItems="center" flexWrap="wrap" color="gray.600" marginTop="12px">
+      <Text display="inline" marginRight="8px">
+        <Box as={StarIcon} display="inline" />
+        {starsCount}
+      </Text>
+      {licenseInfo && <Text marginRight="8px">{licenseInfo.name}</Text>}
+      <Text marginRight="8px">{`Created: ${createdAt}`}</Text>
+    </Box>
+  </Box>
+);
+
+RepositoryItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  login: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  url: PropTypes.string.isRequired,
+  licenseInfo: PropTypes.shape({ name: PropTypes.string }),
+  starsCount: PropTypes.number.isRequired,
+  createdAt: PropTypes.string.isRequired,
+};
+
+RepositoryItem.defaultProps = {
+  description: null,
+  licenseInfo: null,
+};
+
+export default RepositoryItem;
